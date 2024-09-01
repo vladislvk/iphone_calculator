d69cd46fde51827c2146b37e43e29416edb71303
vladislvk <vladislavkim566@gmail.com>
2024-09-01 20:12:53 +0500

    feat:mvp

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var displayText:String = "test"
+    @State private var displayText:String = ""
     
     struct GridItemData: Identifiable {
         let id: Int
@@ -23,7 +23,9 @@ struct ContentView: View {
                 Spacer()
                 LazyVGrid(columns: columns, spacing: 5) {
                     ForEach(0..<20) { index in
-                        Button(action: {}) {
+                        Button(action: {
+                            buttonAction(index: index)
+                        }) {
                             Circle()
                                 .fill(colors[index % colors.count])
                                 .frame(height: 100)
@@ -42,9 +44,39 @@ struct ContentView: View {
     }
     
     
-    func buttonAction(_ index:Int){
+    func buttonAction(index: Int) {
+        let buttonText = textForIndex(index)
         
+        switch buttonText {
+        case "AC":
+            displayText = "0"
+        case "+/-":
+            if let value = Double(displayText) {
+                displayText = String(-value)
+            }
+        case "%":
+            if let value = Double(displayText) {
+                displayText = String(value / 100)
+            }
+        case "/", "x", "-", "+":
+            displayText += " \(buttonText) "
+        case "=":
+            displayText = evaluateExpression(displayText)
+        default:
+            if displayText == "0" && buttonText != "." {
+                displayText = buttonText
+            } else {
+                displayText += buttonText
+            }
+        }
     }
+    func evaluateExpression(_ expression: String) -> String {
+            let exp = NSExpression(format: expression.replacingOccurrences(of: "x", with: "*"))
+            if let result = exp.expressionValue(with: nil, context: nil) as? Double {
+                return String(result)
+            }
+            return "Error"
+        }
 }
 
 #Preview {
